60e81abe8527b329fc7ec278d50ee032b285fc80
jad-zeid <jadzeid99@gmail.com>
2025-09-29 16:15:22 +0300

    Update connect4.h

--- connect4.h
+++ connect4.h
@@ -1,5 +1,8 @@
 #ifndef CONNECT4_H
 #define CONNECT4_H
-
+void printboard(char board[6][7]);
+void drop(char board[6][7], int col, char player, int* count);
+int checkHorizontal(char board[6][7], char player);
+int checkVertical(char board[6][7], char player);
 #endif
 
